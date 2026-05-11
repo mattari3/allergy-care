79c290f8e94dc5d9ac9590a925cf686ef679be35
mattari3 <mattari.manga@gmail.com>
2026-05-12 07:23:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'allergy-care-v2';
+const CACHE = 'allergy-care-v4';
 const ASSETS = ['./','./index.html','./styles.css','./script.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
